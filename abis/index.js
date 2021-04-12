812202b25f391ddbbfa071310b54b363b53125f8
Jorge Samuel <81595296+TecnoGames13@users.noreply.github.com>
2021-04-12 18:22:05 -0500

    Add files via upload

---
+++ abis/index.js
@@ -0,0 +1,9 @@
+const arbitrage = require('./arbitrage.json');
+const uniswap = require('./uniswap.json');
+const sushiswap = require('./sushiswap.json');
+
+module.exports = {
+  arbitrage,
+  uniswap,
+  sushiswap
+};
